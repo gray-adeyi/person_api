3b2b2ecb066e1504e0ede769a64afb2b0010d588
Gbenga Adeyi <adeyigbenga005@gmail.com>
2023-09-10 23:20:16 +0100

    Remove load dotenv

--- db.py
+++ db.py
@@ -4,9 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from sqlmodel import SQLModel
-from dotenv import load_dotenv
 
-load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 database_engine = AsyncEngine(create_engine(DATABASE_URL))
